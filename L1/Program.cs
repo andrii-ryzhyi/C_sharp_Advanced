80a24ba2ff203887bc4d88c1d5053ecfe71308d1
Andrii Ryzhyi <andrii.nrj@gmail.com>
2020-03-11 19:27:01 +0200

    Add project files.

---
+++ L1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1
+{
+    public class Test
+    {
+        public int Age { get; set; }
+        public String Name { get; set; }
+        public double Result { get; set; }
+        public int cnt { get; }
+        public int cnt2 { get; private set; }
+
+        private int count;
+        private string lastName;
+
+        public Test()
+        {
+            Age = 3;
+            Name = "Test";
+            Result = 0.1;
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+    }
+    }
+}
