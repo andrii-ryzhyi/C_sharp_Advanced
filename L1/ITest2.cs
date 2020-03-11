8e53f54411fb2c21022daf80c121f8ae75a8e82c
Andrii Ryzhyi <andrii.nrj@gmail.com>
2020-03-11 20:35:39 +0200

    added interface

---
+++ L1/ITest2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1
+{
+    interface ITest2
+    {
+        void DoWork();
+    }
+}
--- L1/Program.cs
+++ L1/Program.cs
@@ -6,29 +6,77 @@ using System.Threading.Tasks;
 
 namespace L1
 {
-    public class Test
+    public class Test : AbstarctClass, ITest, ITest2
     {
         public int Age { get; set; }
         public String Name { get; set; }
         public double Result { get; set; }
         public int Cnt { get; }
         public int Cnt2 { get; private set; }
+        public string Color { get; set; }
 
-        private int count;
-        private string lastName;
+        private int _count;
+        private string _lastName;
+
+        public static int ObjectsCount;
+
+        public Test(int age, string name, double result)
+        {
+            Age = age;
+            Name = name;
+            Result = result;
+            ObjectsCount++;
+        }
 
         public Test()
         {
-            Age = 3;
-            Name = "Test";
-            Result = 0.1;
+            ObjectsCount++;
+        }
+
+        public int Increase(int first)
+        {
+            return first++;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Call from method Print");
+        }
+
+        public static int GetObjects()
+        {
+            return ObjectsCount;
+        }
+
+        public override void Method1()
+        {
+            Console.WriteLine("Overrided Method1");
+        }
+        public override void Method2()
+        {
+            Console.WriteLine("Overrided Method2");
+        }
+
+        void ITest.DoWork()
+        {
+            Console.WriteLine("Do some work from ITest");
+        }
+        void ITest2.DoWork()
+        {
+            Console.WriteLine("Do some work from ITest2");
         }
     }
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            
-    }
+            //Test test = new Test();
+            int a = 1;
+            int b = a++;
+            int c = ++a;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
     }
 }
